b47fad082ff8558f2b42f23a5061e8be3e41de09
mck0124 <minchan0124@gmail.com>
2026-08-07 17:33:10 +0800

    remove redundant github controller imports

--- backend/src/main/java/com/zoop/backend/controller/GithubSearchController.java
+++ backend/src/main/java/com/zoop/backend/controller/GithubSearchController.java
@@ -12,16 +12,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.zoop.backend.domain.dto.FilterRequestDto;
 import com.zoop.backend.domain.dto.GithubSearchResultDto;
 import com.zoop.backend.domain.dto.GithubSearchResultWithStageDto;
